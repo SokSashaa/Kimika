172b83ee18e210ad11822cd57d85784ea4ab9176
SokSashaa <127789980+SokSashaa@users.noreply.github.com>
2023-06-30 00:19:41 +0300

    fix

--- 22.06/Unity/Assets/_Data/Scripts/DataHolder.cs
+++ 22.06/Unity/Assets/_Data/Scripts/DataHolder.cs
@@ -8,7 +8,7 @@ public static class DataHolder
     public static int Score = 0;
     public static List<string> opened = new List<string>();
     public static List<GameObject> Slot = new List<GameObject>();
-    public static int TargetScore = 999999999;
+    public static int TargetScore = 9999999;
     public static Image ImageTarget;
     public static string NameTarget;
     public static string NameTarget1;
